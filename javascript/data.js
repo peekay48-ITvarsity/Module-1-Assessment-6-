22dd8525f18897bb989e4e2fbd23915bc8fdcc2a
Phakama Nkanyezi Myeni <peekaynkanyezi@gmail.com>
2026-04-16 07:21:14 +0200

    Add files via upload

---
+++ javascript/data.js
@@ -0,0 +1,100 @@
+// ID of categories for which products from the last category, aka accessories,
+// should be lised below the product description on single.html
+categoriesShowAdditionalList = [1,2,3];
+
+categories = [
+    { name: "Category1", image: "grid4.jpg" },
+    { name: "Category2", image: "grid5.jpg" },
+    { name: "Category3", image: "grid6.jpg" },
+    { name: "Category4", image: "grid7.jpg" }
+  ];
+
+products = [
+  // category
+  [
+    // product
+    { name: "Aliquam",
+      description: "Nullam leo tellus",
+      details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies lacinia augue, sed tincidunt lorem gravida vitae. Duis pretium lobortis augue, non mattis magna tincidunt in.",
+      thumbnail: "grid4.jpg",
+      images: ["show.jpg", "show1.jpg", "show2.jpg", "show3.jpg" ],
+      downloads: []
+    },
+    // product
+    { name: "Test2",
+      description: "",
+      details: "",
+      thumbnail: "grid6.jpg",
+      images: ["show.jpg"],
+      downloads: []
+    }
+  ],
+
+  // category
+  [
+    // product
+    { name: "Suspendisse consectetur",
+      description: "",
+      details: "",
+      thumbnail: "grid4.jpg",
+      images: ["show.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid6.jpg",
+      images: ["show.jpg"],
+      downloads: []
+    }
+  ],
+
+  // category
+  [
+    // product
+    { name: "Suspendisse consectetur",
+      description: "",
+      details: "",
+      thumbnail: "grid3.jpg",
+      images: ["show1.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid6.jpg",
+      images: ["show2.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid5.jpg",
+      images: ["show.jpg"],
+      downloads: []
+    },
+    // product
+    { name: "Pellentesque",
+      description: "",
+      details: "",
+      thumbnail: "grid7.jpg",
+      images: ["show.jpg"],
+      downloads: []
+    }
+  ],
+
+  // category
+  [
+    // product
+    { name: "Test",
+      description: "",
+      details: "",
+      thumbnail: "grid4.jpg",
+      images: ["show1.jpg"],
+      downloads: []
+    }
+  ],
+]
